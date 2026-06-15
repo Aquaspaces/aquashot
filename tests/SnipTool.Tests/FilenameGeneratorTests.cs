3d8d643eb98e5d5ddb659c3a3155d6817755b4f2
Aqua <97066993+Aquaspaces@users.noreply.github.com>
2026-06-15 10:17:18 -0700

    feat: add FilenameGenerator with date-pattern substitution

---
+++ tests/SnipTool.Tests/FilenameGeneratorTests.cs
@@ -0,0 +1,29 @@
+using FluentAssertions;
+using SnipTool.Output;
+using Xunit;
+
+namespace SnipTool.Tests;
+
+public class FilenameGeneratorTests
+{
+    private static readonly DateTime Ts = new(2026, 6, 15, 14, 30, 22);
+
+    [Fact]
+    public void Generate_SubstitutesDatePattern()
+    {
+        var name = FilenameGenerator.Generate("Screenshot_{yyyy-MM-dd_HHmmss}", "png", Ts);
+        name.Should().Be("Screenshot_2026-06-15_143022.png");
+    }
+
+    [Fact]
+    public void Generate_AppendsExtensionWhenMissing()
+    {
+        FilenameGenerator.Generate("shot", "png", Ts).Should().Be("shot.png");
+    }
+
+    [Fact]
+    public void Generate_DoesNotDoubleExtension()
+    {
+        FilenameGenerator.Generate("shot.png", "png", Ts).Should().Be("shot.png");
+    }
+}
